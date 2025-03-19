e3056e9f9cd28b1569d33a12348fd92d4635f461
nikhil7827 <ndharmajigar@gmail.com>
2025-03-18 20:49:40 -0500

    chages

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return "Hello, DevOps World!"
 
+
+@app.route('/<name>')
+def print_name(name):
+    return "Hi, {}".format(name)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
